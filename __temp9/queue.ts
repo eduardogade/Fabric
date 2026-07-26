34fa99e7b2d12bebb56e09b9067f2d49c8e6316e
Eduardo Gade Gusmao <eggduzao@gmail.com>
2026-07-26 16:19:57 -0300

    Update GitHub username to eduardogade

--- __temp9/queue.ts
+++
@@ -1,7 +0,0 @@
-export interface Queue<T> {
-  enqueue(item: T): void
-  dequeue(): T | undefined
-  peek(): T | undefined | null
-  isEmpty(): boolean
-  length(): number
-}
--- __trial4/GraphEdge.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class GraphEdge
-{
-    public string $start;
-    public string $end;
-    public int $weight;
-}
